5fc19a217507cd0ce37429348ec00095c8b77cba
Ioankoua <68920364+Ioankoua@users.noreply.github.com>
2023-02-08 20:05:23 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,11 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "", "test");
+$host = "localhost";
+$database = "test";
+$username = "root";
+$password = "";
+
+$conn = new mysqli($host, $username, $password, $database);
 if (!$conn) {
   die("Error: " . mysqli_connect_error());
 }
